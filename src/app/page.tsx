f497a8bfab0103ba59720f8ada35fb12ac2eac29
Tamer Technologies <216339134+Tamer-Technologies@users.noreply.github.com>
2026-02-06 03:36:37 +0200

    feat: create app home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,39 @@
+"use client";
+
 import { Button } from "@/components/ui/button";
+import { homePageData } from "@/constants/pagesContent/homeContent";
+import Link from "next/link";
+import { ComponentRef, useEffect, useRef } from "react";
+import Typed from "typed.js";
 
 export default function Home() {
+  const titleRef = useRef<ComponentRef<"div">>(null);
+
+  useEffect(() => {
+    const typed = new Typed(titleRef.current, {
+      strings: homePageData.titles,
+      typeSpeed: 50,
+      backSpeed: 25,
+      backDelay: 1500,
+      loop: true,
+      loopCount: 4,
+    });
+
+    return () => {
+      typed.destroy();
+    };
+  }, []);
+
   return (
-    <div>
-      <Button>Hello There</Button>
+    <div className="flex flex-col items-center justify-center gap-14">
+      <h1 className="text-4xl sm:text-5xl md:text-6xl lg:text-7xl font-bold text-center mt-10">
+        <span className="sr-only">{homePageData.titles[0]}</span>
+        <span aria-hidden="true" ref={titleRef} />
+      </h1>
+      <p className="max-w-130 text-center">{homePageData.description}</p>
+      <Button className="inline-block" asChild>
+        <Link href={homePageData.cta.url}>{homePageData.cta.label}</Link>
+      </Button>
     </div>
   );
 }
---
+++ src/constants/pagesContent/homeContent.ts
@@ -0,0 +1,20 @@
+type HomePageData = {
+  titles: string[];
+  description: string;
+  cta: {
+    label: string;
+    url: string;
+  };
+};
+
+export const homePageData: HomePageData = {
+  titles: ["Trace Your Jobs", "Organize yourself", "Track Your Progress"],
+  description: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi ad
+        repellendus tenetur officiis, numquam ratione obcaecati ducimus, aperiam
+        odit velit temporibus delectus similique amet vel. Modi delectus eos
+        asperiores suscipit!`,
+  cta: {
+    label: "Trace job",
+    url: "/jobs",
+  },
+};
